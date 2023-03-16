1d3cf6bc2d0aa33450b2a12d160c90458cc03873
w44t3r <vk.educate@protonmail.ch>
2023-03-16 05:01:25 +0100

    styled components/registration & inputs uttils for reg - login.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,16 @@
-import Navbar from "./navbar";
+import Navbar from "./Navbar";
 import './app.css'
+import {BrowserRouter, Route} from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-    </div>
+      <BrowserRouter>
+          <div className="App">
+              <Navbar/>
+              <Switch>
+                  <Route path="/registration" component={Registration}/>
+              </Switch>
+          </div>
+      </BrowserRouter>
   );
 }
 
