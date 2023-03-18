09ba2f6ef402aab16210e57adb187491d4bc31ac
w44t3r <vk.educate@protonmail.ch>
2023-03-18 22:11:47 +0100

    fixed dispatch

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,7 +2,8 @@ import Navbar from "./Navbar";
 import './app.css'
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import React from "react";
-import Registration from "./registration/Registration";
+import Registration from "./auth/Registration";
+import Login from "./auth/login";
 function App() {
   return (
       <BrowserRouter>
@@ -10,6 +11,7 @@ function App() {
               <Navbar/>
               <Routes>
                   <Route path="/registration" element={<Registration/>}/>
+                  <Route path="/login" element={<Login/>}/>
               </Routes>
           </div>
       </BrowserRouter>
--- client/src/components/auth/login.jsx
+++ client/src/components/auth/login.jsx
@@ -1,18 +1,23 @@
 import React, {useState} from "react";
 import './login.css'
 import Input from "../../uttils/Input";
+import {login} from "../../action/user";
+import {useDispatch} from "react-redux";
 
 
 const Login = () => {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const dispatch = useDispatch()
+
+
 
     return (
         <div className='login'>
             <div className="login__header">Log In</div>
             <Input value={email} setValue={setEmail} type="text" placeholder="Введите email..."/>
             <Input value={password} setValue={setPassword} type="password" placeholder="Введите пароль..."/>
-            <button className="login__btn">Войти</button>
+            <button className="login__btn" onClick={() => dispatch(login(email, password))}>Войти</button>
         </div>
     );
 };
