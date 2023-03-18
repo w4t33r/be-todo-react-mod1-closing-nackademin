37a6761905033e286a427675c9e2fb66ee24b590
w44t3r <vk.educate@protonmail.ch>
2023-03-18 22:11:03 +0100

    fixed dispatch, login working

--- client/src/action/user.js
+++ client/src/action/user.js
@@ -6,10 +6,27 @@ export const registration = async (email, password) => {
             email,
             password
         })
-        alert(response)
         alert((await response).data.message)
     } catch (e) {
-        alert(e)
-        console.log(e)
+        alert(e.response.data.message)
+    }
+}
+
+
+
+
+
+export const login =  (email, password) => {
+    return async dispatch => {
+        try {
+            const url = "http//localhost:5000/api/auth/login"
+            const response = await axios.post('http://localhost:5000/api/auth/login', {
+                email,
+                password
+            })
+            console.log(response.data)
+        } catch (e) {
+            alert(e)
+        }
     }
 }
