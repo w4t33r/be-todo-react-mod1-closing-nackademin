66f18187ed883735f97ba6c12e3097ccbfcb46ee
w44t3r <vk.educate@protonmail.ch>
2023-03-18 16:13:54 +0100

    fixed cors https, switch to http

--- client/src/action/user.js
+++ client/src/action/user.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const registration = async (email, password) => {
     try {
-        const response = await axios.post('https://localhost:5000/api/auth/registration',{
+        const response = await axios.post('http://localhost:5000/api/auth/registration',{
             email,
             password
         })
--- client/src/uttils/Input.jsx
+++ client/src/uttils/Input.jsx
@@ -3,11 +3,10 @@ import './input.css'
 
 const Input = (props) => {
     return (
-        <input onChange={(event)=> props.setValue(event.target.value)}
+        <input onChange={(event) => props.setValue(event.target.value)}
                value={props.value}
                type={props.type}
                placeholder={props.placeholder}/>
     );
 };
-
 export default Input;
