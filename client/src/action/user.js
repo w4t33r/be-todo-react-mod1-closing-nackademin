63dc5af7ff2ccb3fe356a5bd86d7b370d3311b9e
w44t3r <vk.educate@protonmail.ch>
2023-03-18 15:31:40 +0100

    bugged cors

--- client/src/action/user.js
+++ client/src/action/user.js
@@ -2,12 +2,14 @@ import axios from "axios";
 
 export const registration = async (email, password) => {
     try {
-        const response = await axios.post('https://localhost:5000/api/auth/registration', {
+        const response = await axios.post('https://localhost:5000/api/auth/registration',{
             email,
             password
         })
-        alert(response.data.message)
+        alert(response)
+        alert((await response).data.message)
     } catch (e) {
         alert(e)
+        console.log(e)
     }
 }
