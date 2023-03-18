8b12e29b670225438c04edf1e8302f96d20de621
w44t3r <vk.educate@protonmail.ch>
2023-03-19 00:10:06 +0100

    logout, btn fixes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
 import './navbar.css'
 import {NavLink} from "react-router-dom";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {logout} from "../reducer/userReducer";
 
 const Navbar = () => {
     const isAuth = useSelector(state => state.user.isAuth)
+    const dispatch = useDispatch()
     return (
         <div className="navbar">
             <div className="container">
                 <div className="navbar__header">Backend_1_todoapp</div>
-                {!isAuth &&<div className="navbar__login"><NavLink to="/login"> login </NavLink></div>}
-                {!isAuth &&<div className="navbar__reg"><NavLink to="/registration"> registration </NavLink></div>}
-                {isAuth &&<div className="navbar__reg"><NavLink to="/registration"> exit </NavLink></div>}
+                {!isAuth && <div className="navbar__login"><NavLink to="/login"> login </NavLink></div>}
+                {!isAuth && <div className="navbar__reg"><NavLink to="/registration"> registration </NavLink></div>}
+                {isAuth && <div className="navbar__button" onClick={() => dispatch(logout())}> Exit </div>}
             </div>
         </div>
-
-
-
-
 
 
     );
