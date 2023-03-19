d59863a4f785a86bb5a7b24c1e996acd2d507b1a
w44t3r <vk.educate@protonmail.ch>
2023-03-19 21:06:25 +0100

    routes to todoList, default route with auth api/list/save

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require("mongoose");
 const authRouter = require("./routes/auth")
+const todoRoute = require('./routes/listRoute')
 const cors = require('./middleware/corsMiddleware')
 require('dotenv').config()
 
@@ -13,7 +14,7 @@ app.use(cors)
 app.use(express.json())
 
 app.use("/api/auth", authRouter)
-
+app.use("/api/list", todoRoute)
 
 const start = async () => {
     try {
